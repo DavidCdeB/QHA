55e4ce8032fd068dcbaef890bb7c82ca994955c5
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2017-06-18 19:23:43 +0100

    Add files via upload

---
+++ moving.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+ mv   G_vs_P_at_Temperature_1     G_vs_P_at_Temperature_10.                                                
+ mv   G_vs_P_at_Temperature_2     G_vs_P_at_Temperature_30.1                                               
+ mv   G_vs_P_at_Temperature_3     G_vs_P_at_Temperature_50.2                                               
+ mv   G_vs_P_at_Temperature_4     G_vs_P_at_Temperature_70.3                                               
+ mv   G_vs_P_at_Temperature_5     G_vs_P_at_Temperature_90.4                                               
+ mv   G_vs_P_at_Temperature_6     G_vs_P_at_Temperature_110.5                                              
+ mv   G_vs_P_at_Temperature_7     G_vs_P_at_Temperature_130.6                                              
+ mv   G_vs_P_at_Temperature_8     G_vs_P_at_Temperature_150.7                                              
+ mv   G_vs_P_at_Temperature_9     G_vs_P_at_Temperature_170.8                                              
+ mv   G_vs_P_at_Temperature_10    G_vs_P_at_Temperature_190.9                                              
+ mv   G_vs_P_at_Temperature_11    G_vs_P_at_Temperature_211.                                               
+ mv   G_vs_P_at_Temperature_12    G_vs_P_at_Temperature_231.1                                              
+ mv   G_vs_P_at_Temperature_13    G_vs_P_at_Temperature_251.2                                              
+ mv   G_vs_P_at_Temperature_14    G_vs_P_at_Temperature_271.3                                              
+ mv   G_vs_P_at_Temperature_15    G_vs_P_at_Temperature_291.4                                              
+ mv   G_vs_P_at_Temperature_16    G_vs_P_at_Temperature_311.5                                              
+ mv   G_vs_P_at_Temperature_17    G_vs_P_at_Temperature_331.6                                              
+ mv   G_vs_P_at_Temperature_18    G_vs_P_at_Temperature_351.7                                              
+ mv   G_vs_P_at_Temperature_19    G_vs_P_at_Temperature_371.8                                              
+ mv   G_vs_P_at_Temperature_20    G_vs_P_at_Temperature_391.9                                              
+ mv   G_vs_P_at_Temperature_21    G_vs_P_at_Temperature_412.                                               
+ mv   G_vs_P_at_Temperature_22    G_vs_P_at_Temperature_432.1                                              
+ mv   G_vs_P_at_Temperature_23    G_vs_P_at_Temperature_452.2                                              
+ mv   G_vs_P_at_Temperature_24    G_vs_P_at_Temperature_472.3                                              
+ mv   G_vs_P_at_Temperature_25    G_vs_P_at_Temperature_492.4                                              
+ mv   G_vs_P_at_Temperature_26    G_vs_P_at_Temperature_512.5                                              
+ mv   G_vs_P_at_Temperature_27    G_vs_P_at_Temperature_532.6                                              
+ mv   G_vs_P_at_Temperature_28    G_vs_P_at_Temperature_552.7                                              
+ mv   G_vs_P_at_Temperature_29    G_vs_P_at_Temperature_572.8                                              
+ mv   G_vs_P_at_Temperature_30    G_vs_P_at_Temperature_592.9                                              
+ mv   G_vs_P_at_Temperature_31    G_vs_P_at_Temperature_613.                                               
+ mv   G_vs_P_at_Temperature_32    G_vs_P_at_Temperature_633.1                                              
+ mv   G_vs_P_at_Temperature_33    G_vs_P_at_Temperature_653.2                                              
+ mv   G_vs_P_at_Temperature_34    G_vs_P_at_Temperature_673.3                                              
+ mv   G_vs_P_at_Temperature_35    G_vs_P_at_Temperature_693.4                                              
+ mv   G_vs_P_at_Temperature_36    G_vs_P_at_Temperature_713.5                                              
+ mv   G_vs_P_at_Temperature_37    G_vs_P_at_Temperature_733.6                                              
+ mv   G_vs_P_at_Temperature_38    G_vs_P_at_Temperature_753.7                                              
+ mv   G_vs_P_at_Temperature_39    G_vs_P_at_Temperature_773.8                                              
+ mv   G_vs_P_at_Temperature_40    G_vs_P_at_Temperature_793.9                                              
+ mv   G_vs_P_at_Temperature_41    G_vs_P_at_Temperature_814.                                               
+ mv   G_vs_P_at_Temperature_42    G_vs_P_at_Temperature_834.1                                              
+ mv   G_vs_P_at_Temperature_43    G_vs_P_at_Temperature_854.2                                              
+ mv   G_vs_P_at_Temperature_44    G_vs_P_at_Temperature_874.3                                              
+ mv   G_vs_P_at_Temperature_45    G_vs_P_at_Temperature_894.4                                              
+ mv   G_vs_P_at_Temperature_46    G_vs_P_at_Temperature_914.5                                              
+ mv   G_vs_P_at_Temperature_47    G_vs_P_at_Temperature_934.6                                              
+ mv   G_vs_P_at_Temperature_48    G_vs_P_at_Temperature_954.7                                              
+ mv   G_vs_P_at_Temperature_49    G_vs_P_at_Temperature_974.8                                              
+ mv   G_vs_P_at_Temperature_50    G_vs_P_at_Temperature_994.9                                              
+ mv   G_vs_P_at_Temperature_51    G_vs_P_at_Temperature_1015.                                              
+ mv   G_vs_P_at_Temperature_52    G_vs_P_at_Temperature_1035.1                                             
+ mv   G_vs_P_at_Temperature_53    G_vs_P_at_Temperature_1055.2                                             
+ mv   G_vs_P_at_Temperature_54    G_vs_P_at_Temperature_1075.3                                             
+ mv   G_vs_P_at_Temperature_55    G_vs_P_at_Temperature_1095.4                                             
+ mv   G_vs_P_at_Temperature_56    G_vs_P_at_Temperature_1115.5                                             
+ mv   G_vs_P_at_Temperature_57    G_vs_P_at_Temperature_1135.6                                             
+ mv   G_vs_P_at_Temperature_58    G_vs_P_at_Temperature_1155.7                                             
+ mv   G_vs_P_at_Temperature_59    G_vs_P_at_Temperature_1175.8                                             
+ mv   G_vs_P_at_Temperature_60    G_vs_P_at_Temperature_1195.9                                             
+ mv   G_vs_P_at_Temperature_61    G_vs_P_at_Temperature_1216.                                              
+ mv   G_vs_P_at_Temperature_62    G_vs_P_at_Temperature_1236.1                                             
+ mv   G_vs_P_at_Temperature_63    G_vs_P_at_Temperature_1256.2                                             
+ mv   G_vs_P_at_Temperature_64    G_vs_P_at_Temperature_1276.3                                             
+ mv   G_vs_P_at_Temperature_65    G_vs_P_at_Temperature_1296.4                                             
+ mv   G_vs_P_at_Temperature_66    G_vs_P_at_Temperature_1316.5                                             
+ mv   G_vs_P_at_Temperature_67    G_vs_P_at_Temperature_1336.6                                             
+ mv   G_vs_P_at_Temperature_68    G_vs_P_at_Temperature_1356.7                                             
+ mv   G_vs_P_at_Temperature_69    G_vs_P_at_Temperature_1376.8                                             
+ mv   G_vs_P_at_Temperature_70    G_vs_P_at_Temperature_1396.9                                             
+ mv   G_vs_P_at_Temperature_71    G_vs_P_at_Temperature_1417.                                              
+ mv   G_vs_P_at_Temperature_72    G_vs_P_at_Temperature_1437.1                                             
+ mv   G_vs_P_at_Temperature_73    G_vs_P_at_Temperature_1457.2                                             
+ mv   G_vs_P_at_Temperature_74    G_vs_P_at_Temperature_1477.3                                             
+ mv   G_vs_P_at_Temperature_75    G_vs_P_at_Temperature_1497.4                                             
+ mv   G_vs_P_at_Temperature_76    G_vs_P_at_Temperature_1517.5                                             
+ mv   G_vs_P_at_Temperature_77    G_vs_P_at_Temperature_1537.6                                             
+ mv   G_vs_P_at_Temperature_78    G_vs_P_at_Temperature_1557.7                                             
+ mv   G_vs_P_at_Temperature_79    G_vs_P_at_Temperature_1577.8                                             
+ mv   G_vs_P_at_Temperature_80    G_vs_P_at_Temperature_1597.9                                             
+ mv   G_vs_P_at_Temperature_81    G_vs_P_at_Temperature_1618.                                              
+ mv   G_vs_P_at_Temperature_82    G_vs_P_at_Temperature_1638.1                                             
+ mv   G_vs_P_at_Temperature_83    G_vs_P_at_Temperature_1658.2                                             
+ mv   G_vs_P_at_Temperature_84    G_vs_P_at_Temperature_1678.3                                             
+ mv   G_vs_P_at_Temperature_85    G_vs_P_at_Temperature_1698.4                                             
+ mv   G_vs_P_at_Temperature_86    G_vs_P_at_Temperature_1718.5                                             
+ mv   G_vs_P_at_Temperature_87    G_vs_P_at_Temperature_1738.6                                             
+ mv   G_vs_P_at_Temperature_88    G_vs_P_at_Temperature_1758.7                                             
+ mv   G_vs_P_at_Temperature_89    G_vs_P_at_Temperature_1778.8                                             
+ mv   G_vs_P_at_Temperature_90    G_vs_P_at_Temperature_1798.9                                             
+ mv   G_vs_P_at_Temperature_91    G_vs_P_at_Temperature_1819.                                              
+ mv   G_vs_P_at_Temperature_92    G_vs_P_at_Temperature_1839.1                                             
+ mv   G_vs_P_at_Temperature_93    G_vs_P_at_Temperature_1859.2                                             
+ mv   G_vs_P_at_Temperature_94    G_vs_P_at_Temperature_1879.3                                             
+ mv   G_vs_P_at_Temperature_95    G_vs_P_at_Temperature_1899.4                                             
+ mv   G_vs_P_at_Temperature_96    G_vs_P_at_Temperature_1919.5                                             
+ mv   G_vs_P_at_Temperature_97    G_vs_P_at_Temperature_1939.6                                             
+ mv   G_vs_P_at_Temperature_98    G_vs_P_at_Temperature_1959.7                                             
+ mv   G_vs_P_at_Temperature_99    G_vs_P_at_Temperature_1979.8                                             
+ mv   G_vs_P_at_Temperature_100   G_vs_P_at_Temperature_2000.0                                             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ plot_palette.gnu
@@ -0,0 +1,42 @@
+#
+
+#
+set termoption enhanced
+set encoding utf8 # uncomment to save eps
+
+
+set term post enh color eps # uncomment to save eps
+
+
+set output "Calcite_I_plot_palette.eps" # uncomment to save eps
+
+
+#set title "Calcite I"  font  ", 20"
+set ylabel "Pressure (GPa)" font  ", 15" rotate parallel
+set zlabel "Gibbs free energy/Formula Unit (a.u.)"  font  ", 13 "  offset -3,0,0  rotate parallel
+set xlabel 'Temperature (K)' font  ", 15" offset -3,0,0  rotate parallel
+
+#set xrange [124:138]
+#set yrange [-1883.030:-1883.000]
+set xtics font ", 15" # offset 0,1 border 1,1,1
+
+set ytics font ", 15"
+set key font ",15" # Changes the font of the letters of the legend
+
+#set key outside
+set ticslevel 0
+
+set palette defined ( 0 "blue", 0.5 "light-grey", 1 "red" )
+
+set grid
+#set dgrid3d 30,30 gauss 
+set view 60, 30, 1, 1
+# splot "grid" using 1:2:3 with lp lt 1 pt 7  #point type YY
+
+splot "Thermo_functions_with_P_and_G_sorted_with_EL_from_EOS__VOLUME_and_All_energies_divided_by_F_unit_SORTED_in_Ts_blocked.dat" using 11:3:10 w pm3d title "" # option in the book
+#splot "Thermo_functions_with_P_and_G_sorted_with_EL_from_EOS__VOLUME_and_All_energies_divided_by_F_unit.dat" using 11:3:10 with points lc 2 title "" #original scatter (green)
+
+set output "Calcite_I_plot_palette.eps" # uncomment to save eps
+
+replot # uncomment to save eps
+
